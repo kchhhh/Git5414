93dbae6bfb42ff907c74aa3ae2bc9cf32f205a5c
kch <chris9712@naver.com>
2025-04-07 20:43:23 +0900

    Modify mynumber.c

--- mynumber.c
+++ mynumber.c
@@ -2,7 +2,11 @@
 
 int main() {
   for (int i = 1; i <= 100; i++) {
-    if (i % 2) printf("%d\n", i)
+    if (i % 2) printf("%d\n", i);
+}
+  printf("\n"
+  for (int i = 1; i <= 100; i++) {
+    if (i % 2 == 0) printf("%d\n", i);
 }
   return 0;
 }
